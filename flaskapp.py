710109dd7fdab028b0862e1fb5a758e17c62db36
sndnyang <sndnyangd@gmail.com>
2016-02-24 15:24:33 +0800

    configured flask-login loginmanager

--- flaskapp.py
+++ flaskapp.py
@@ -3,8 +3,19 @@ from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
 
+from flask.ext.login import LoginManager
+
+from app import *
+
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 @app.route('/index')
@@ -23,6 +34,20 @@ def test():
 @app.route('/login.html')
 def login():
     return render_template('login.html')
+
+@app.route('/signup')
+@app.route('/signup.html')
+@app.route('/register.html')
+def signup():
+    code_img,strs = create_validate_code() 
+    buf = StringIO.StringIO() 
+    code_img.save(buf,'JPEG',quality=70) 
+ 
+    buf_str = buf.getvalue() 
+   #response = app.make_response(buf_str)  
+   #response.headers['Content-Type'] = 'image/jpeg'  
+   #return response
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug = True)

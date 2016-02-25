1bce85770d6643376ec593e76513e2342349b985
sndnyang <sndnyangd@gmail.com>
2016-02-25 18:05:03 +0800

    setup.py update

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='FlaskApp',
       author='sndnyang',
       author_email='sndnyangd@gmail.com',
       url='http://sndnyang.github.io',
-     install_requires=['Flask>=0.10.1', 'flask-login==0.2.7', 'sqlalchemy==0.8.2', 'flask-sqlalchemy==1.0' ],
+     install_requires=['Flask==0.10.1', 'Flask-Login==0.3.2', 'Flask-OAuthlib==0.9.2', 'Flask-SQLAlchemy==2.1', 'Flask-WTF==0.12', 'itsdangerous==0.24', 'Jinja2==2.8', 'MarkupSafe==0.23', 'oauthlib==1.0.3', 'Pillow==3.1.1', 'psycopg2==2.6.1', 'python-openid==2.2.5', 'requests==2.9.1', 'requests-oauthlib==0.6.0', 'SQLAlchemy==1.0.12', 'Werkzeug==0.11.4', 'wheel==0.29.0', 'WTForms==2.1'
+         ],
      )

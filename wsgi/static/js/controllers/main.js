6207621730dba4d7a2562a1b0c822bffa678c7c5
sndnyang <sndnyangd@gmail.com>
2016-02-26 18:11:21 +0800

    主要完成功能 1. map 和 主页分离 2. 个人主页链接 3. 后台取数据 4. 用户主页

--- wsgi/static/js/controllers/main.js
+++ wsgi/static/js/controllers/main.js
@@ -2,15 +2,30 @@
 
 var module = angular.module('controller', []);
 
-module.controller('MainCtrl', [ '$scope' , function ($scope) {
+module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.root = null;		
     $scope.fileName = "mindMap";
+    
+    
+    $scope.init = function(data){
+        var url = "/static/data/data.json";
+        if (data != "index")
+            url = "/loadmap/" + data;
 
-    d3.json("/static/data/data.json", function(json) {			  	
-        $scope.json = json;
-        $scope.$apply();
-    });
+        d3.json(url, function(error, json) {
+            if (error) {
+                alert('遇到问题了');
+            }
+            if ('error' in json && json.error == 'not exist') {
+                alert('该导图不存在，返回');
+                window.history.back(-1); 
+                return ;
+            }
+            $scope.json = json;
+            $scope.$apply();
+        });
+    } 
 
     function serializeData(source){
         var json = {};		
@@ -75,7 +90,9 @@ module.controller('MainCtrl', [ '$scope' , function ($scope) {
         var saveData = serializeData($scope.root);
         // window.open("data:text/json;charset=utf-8," + escape(JSON.stringify(saveData)));		   
         var MIME_TYPE = 'application/json';
-        var bb = new Blob([JSON.stringify(saveData)], {type: MIME_TYPE});
+        //var bb = new Blob([JSON.stringify(saveData)], {type: MIME_TYPE});
+        var jsonData = JSON.stringify({'title': $scope.root['name'], 'data':saveData});
+        var bb = new Blob([jsonData], {type: MIME_TYPE});
 
         var a = document.createElement('a');
         a.download = $scope.fileName + ".json";
@@ -85,6 +102,15 @@ module.controller('MainCtrl', [ '$scope' , function ($scope) {
         a.dataset.downloadurl = [MIME_TYPE, a.download, a.href].join(':');
         document.querySelectorAll("#downloadLinkWrap")[0].innerHTML = "";
         document.querySelectorAll("#downloadLinkWrap")[0].appendChild(a);  
+
+        $http.post('/save', jsonData).success(function(data){
+            $scope.msg = 'Data saved';
+            alert("success message:" + data);
+        })
+        .error(function(data) {
+            alert("failure message:" + JSON.stringify({data:data}));
+        });
+
     }
 
 }]);
@@ -187,7 +213,7 @@ function update(source) {
                 var ele = d.link[i];
                 var name = ele['name'];
                 var url = ele['url'];
-                text += name + ': <a href="' + url + '" target="_blank">'+name +' </a></br>';
+                text += '<a href="' + url + '" target="_blank">'+name +' </a></br>';
             }
             if (text != "") {
                 toolTip.html(text)

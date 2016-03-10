28a0ede3566afbc04ae0a5e997484f7e6e69040a
sndnyang <sndnyangd@gmail.com>
2016-03-10 21:06:34 +0800

    排序首版

---
+++ wsgi/static/js/canvas.js
@@ -0,0 +1,26 @@
+function updateData() {
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+
+    network.setData(data);
+    network.redraw();
+}
+
+function markNodes(nodes_list) {
+
+    if (nodes_list.length == 0) {
+        return;
+    }
+
+    for (var i = 0; i < nodes.length; i++) {
+        nodes[i].color = "orange";
+    }
+
+    for (var i in nodes_list) {
+        nodes[nodes_list[i]].color = "red";
+    }
+    
+    updateData();    
+}

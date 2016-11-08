37658dddde7b1d8b64f90b95d4568a88fb6824f7
sndnyang <sndnyangd@gmail.com>
2016-11-09 01:04:14 +0800

    login 和 register 页面调整

---
+++ wsgi/static/js/placeholder.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	
+	$("#username").attr("placeholder", "请输入用户名...");
+    $("#email").attr("placeholder", "请输入email...");
+    $("#password").attr("placeholder", "请输入密码...");
+	$("#confirm").attr("placeholder", "请确认密码...");
+	$("#verification_code").attr("placeholder", "请输入验证码...");
+});

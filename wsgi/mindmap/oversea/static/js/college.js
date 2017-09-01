38109b465b432a7ae7f3bf68606eab7f381fe0a6
sndnyang <sndnyangd@gmail.com>
2017-09-01 22:30:22 +0800

    忘了跳转了

--- wsgi/mindmap/oversea/static/js/college.js
+++ wsgi/mindmap/oversea/static/js/college.js
@@ -617,8 +617,8 @@ function submitRedirect(obj, type, url) {
                 return;
             }
             console.log($("#approveIt").val())
-            // if (type != "research" || (type == "research" && $("approveIt").val() == 1)) {
-            if (type != "research") {
+            if (type != "research" || (type == "research" && $("approveIt").val() == 1)) {
+            // if (type != "research") {
                 alert('请等待审核，准备跳转...');
                 window.location.href = "{0}.html".format(url);
             } else {

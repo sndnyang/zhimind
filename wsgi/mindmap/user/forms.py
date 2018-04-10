ab05562d79aa140356b7322a1cacdf44598ebe2f
sndnyang <sndnyangd@gmail.com>
2018-04-10 13:41:17 -0400

    use wrong forms.py file

--- wsgi/mindmap/user/forms.py
+++ wsgi/mindmap/user/forms.py
@@ -1,18 +1,21 @@
 ﻿# coding=utf-8 
 
-from wtforms import Form, PasswordField, SubmitField, StringField, BooleanField, validators
+from wtforms import Form, PasswordField, SubmitField,\
+    StringField, validators
 
 
-class AccountForm(Form):
-    appname = StringField(u'应用名', [validators.Length(min=4, max=50)])
+class RegistrationForm(Form):
     username = StringField(u'用户名', [validators.Length(min=4, max=25)])
-    hint = StringField(u'提示', [validators.Length(min=6, max=100)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField(u'密码', [
         validators.Length(min=8, max=50),
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField(u'确认密码')
-    cover = BooleanField('覆盖密码')
-    submit = SubmitField(u'记录')
-
+    code = StringField(u'验证码', validators=
+					   [validators.DataRequired(), 
+					    validators.Length(4, 4, message=u'填写4位验证码')])
+    # recaptcha = RecaptchaField()
+    submit = SubmitField(u'注册')
+    # accept_tos = BooleanField('I accept the TOS', [validators.Required()])
